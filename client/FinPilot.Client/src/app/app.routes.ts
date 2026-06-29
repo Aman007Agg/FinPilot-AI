1849c4a2dc3ff78c43f64bb0d499ee310c7c18c0
Aman Agrawal <aman007agg@gmail.com>
2026-06-29 18:50:40 +0530

    feat(client): setup Angular Material, authentication module and application routing

--- client/FinPilot.Client/src/app/app.routes.ts
+++ client/FinPilot.Client/src/app/app.routes.ts
@@ -1,10 +1,29 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { ForgotPassword } from './features/auth/forgot-password/forgot-password';
+
 import { MainLayout } from './layouts/main-layout/main-layout';
 import { Dashboard } from './features/dashboard/dashboard';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'login',
+        component: Login
+    },
+
+    {
+        path: 'register',
+        component: Register
+    },
+
+    {
+        path: 'forgot-password',
+        component: ForgotPassword
+    },
+
+    {
+        path: 'dashboard',
         component: MainLayout,
         children: [
             {
@@ -12,5 +31,17 @@ export const routes: Routes = [
                 component: Dashboard
             }
         ]
+    },
+
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+
+    },
+
+    {
+        path: '**',
+        redirectTo: ''
     }
 ];
